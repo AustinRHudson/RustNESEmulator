cd1f01edda76b1f39fe68ff7f9631090d6f1f1a0
Austin R Hudson <hudsonaustin25@gmail.com>
2025-01-14 17:30:31 -0600

    Test rom function and finished trace

--- src/cartridge.rs
+++ src/cartridge.rs
@@ -55,3 +55,48 @@ impl Rom {
 			})
 	}
 }
+
+pub mod test {
+    use super::*;
+
+    struct test_rom {
+        header: Vec<u8>,
+        trainer: Option<Vec<u8>>,
+        pgp_rom: Vec<u8>,
+        chr_rom: Vec<u8>,
+    }
+
+    fn create_rom(rom: test_rom) -> Vec<u8>{
+        let mut result = Vec::with_capacity(
+            rom.header.len()
+                + rom.trainer.as_ref().map_or(0, |t| t.len())
+                + rom.pgp_rom.len()
+                + rom.chr_rom.len(),
+        );
+
+        result.extend(&rom.header);
+        if let Some(t) = rom.trainer {
+            result.extend(t);
+        }
+        result.extend(&rom.pgp_rom);
+        result.extend(&rom.chr_rom);
+
+        return result;
+    }
+
+    pub fn test_rom(program: Vec<u8>) -> Rom {
+        let mut pgp_rom_contents = program;
+        pgp_rom_contents.resize(2 * PRG_ROM_PAGE_SIZE, 0);
+
+        let test_rom = create_rom(test_rom {
+            header: vec![
+                0x4E, 0x45, 0x53, 0x1A, 0x02, 0x01, 0x31, 00, 00, 00, 00, 00, 00, 00, 00, 00,
+            ],
+            trainer: None,
+            pgp_rom: pgp_rom_contents,
+            chr_rom: vec![2; 1 * CHR_ROM_PAGE_SIZE],
+        });
+
+        Rom::new(&test_rom).unwrap()
+    }
+}
